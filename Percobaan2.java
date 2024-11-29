5d78dc16e291bfaf23da9512613abfabd0869319
ocaocuoci <hernandar0606@gmail.com>
2024-11-29 08:34:54 +0700

    percobaan 2 pertanyaan 2

--- Percobaan2.java
+++ Percobaan2.java
@@ -4,9 +4,15 @@ public class Percobaan2{
         if (y == 0){
             return (1);
         } else {
+            if (y > 1){
+                System.out.print(x + " * ");
+            } else {
+                System.out.print(x);
+            }
             return (x * hitungPangkat(x, y - 1));
         }
     }
+    
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
 
@@ -18,6 +24,9 @@ public class Percobaan2{
         System.out.print("Pangkat: ");
         pangkat = sc.nextInt();
 
-        System.out.println(hitungPangkat(bilangan, pangkat));
+        int hasil = hitungPangkat(bilangan, pangkat);
+        System.out.println("= " + hasil);
+
+        sc.close();
     }
 }
